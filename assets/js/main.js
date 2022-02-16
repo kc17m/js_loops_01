d15139e27df37def94b90ba25403fc7d9c4c1488
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-02-16 16:24:42 +0100

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,6 +99,7 @@ let outputLoop = document.getElementById("outputLoop");
 
 console.log(num, outputLoop);
 let o = "";
+let n = "0";
 let loop = () => {
     console.log("test");
     for (let i = 0; i < inputNum.value; i++) {
@@ -107,6 +108,17 @@ let loop = () => {
     }
 
     outputLoop.innerHTML = `L${o}p`;
+
+    if (Number(inputNum.value) == 0) {
+        outputLoop.innerHTML = `Sorry, 0 is not allowed`
+    }
+    else if (Number(inputNum.value) % 2 !== 0) {
+        for (let i = 0; i < inputNum.value; i++) {
+            n += "0";
+            console.log(n);
+            outputLoop.innerHTML = `L${n}p`;
+        }
+    }
 }
 
 // lev2_1
@@ -126,6 +138,54 @@ for (let i = 1; i <= 100; i++) {
     // console.log(arrHundret);
 }
 console.log(arrHundret);
+
+
+// lev3_1
+
+let text = `I didn't ask for a completely reasonable excuse! I asked you to get busy! But I know you in the future. I cleaned your poop. What are you hacking off? Is it my torso?! 'It is!' My precious torso! Please, Don-Bot… look into your hard drive, and open your mercy file! Shut up and get to the point! I barely knew Philip, but as a clergyman I have no problem telling his most intimate friends all about him. You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. Moving along… Well, then good news! It's a suppository. I'll get my kit! And when we woke up, we had these bodies. Daddy Bender, we're hungry. You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. Alright, let's mafia things up a bit. Joey, burn down the ship. Clamps, burn down the crew. That could be 'my' beautiful soul sitting naked on a couch. If I could just learn to play this stupid thing. Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? I was all of history's great robot actors - Acting Unit 0.8; Thespomat; David Duchovny! Bender, I didn't know you liked cooking. That's so cute. Fatal. You don't know how to do any of those. Who are you, my warranty?! Shut up and get to the point! Fry, you can't just sit here in the dark listening to classical music. That's not soon enough! And why did 'I' have to take a cab? Bender?! You stole the atom. You know, I was God once. For the last time, I don't like lilacs! Your 'first' wife was the one who liked lilacs! And yet you haven't said what I told you to say! How can any of us trust you? Your best is an idiot! I found what I need. And it's not friends, it's things. No! The kind with looting and maybe starting a few fires! When will that be? Ugh, it's filthy! Why not create a National Endowment for Strip Clubs while we're at it? I'm just glad my fat, ugly mama isn't alive to see this day. Why did you bring us here? And until then, I can never die? I'm sure those windmills will keep them cool. You don't know how to do any of those. Fry, you can't just sit here in the dark listening to classical music. You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Oh, but you can. But you may have to metaphorically make a deal with the devil. And by "devil", `
+
+// console.log(text);
+
+let separate = (str) => {
+    let tempArr = str.split("");
+    console.log(tempArr);
+    for (let i = 1; i <= tempArr.length; i++) {
+        if (i % 100 == 0) {
+            tempArr[i] = " - " + i / 100 + " of gesamte-nachrichten";
+            console.log(tempArr[i]);
+        }
+    }
+    console.log(tempArr);
+}
+separate(text)
+
+//lev3_2
+
+let numArr2 = [5, 22, 15, 100, 55];
+
+let divisor = document.getElementById("divisor");
+
+let div = (nums) => {
+    for (let i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+        for (let j = 2; j < nums[i]; j++) {
+            // console.log(nums[i] / j);
+            if (Number.isInteger(nums[i] / j)) {
+                console.log(nums[i] + " ist teilbar durch " + j);
+                divisor.innerHTML += `${nums[i]} is divisible by ${j} <br>`
+            }
+        }
+    }
+}
+
+div(numArr2);
+
+
+
+
+
+
+
 
 
 
